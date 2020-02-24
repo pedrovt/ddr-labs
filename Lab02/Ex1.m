00d0d6ff83c28d6705bdbb7fe318c365a5b0d311
Pedro Teixeira <pedro.teix@ua.pt>
2020-02-24 16:29:55 +0000

    Updates on Guide 2

--- Lab02/Ex1.m
+++ Lab02/Ex1.m
@@ -2,6 +2,7 @@
 % Exercise 1a
 % Given info
 clc
+clear
 
 probs = [ 0.99; 0.999; 0.9999; 0.99999];
 n_rate = 10^-7;
@@ -17,25 +18,24 @@ p_i = 1 - (nchoosek(n, i) * i_rate ^i * (1-i_rate)^(n-i));          % probabilit
 p_e = p_n * probs + p_i * (1-probs);
 
 % obtain p(error and normal/interference and error) | p(error)
-fprintf("--------------")
-p_ns = p_n*probs ./ p_e
-fprintf("--------------")
-p_is = p_i*(1-probs) ./ p_e
-
+p_ns = p_n*probs ./ p_e;                        % probability of link in normal state
+p_is = p_i*(1-probs) ./ p_e;                    % probability of link in interference state
 
 % Exercise 1b and %1c
-
+format compact
 for i = 2:5
-    p_falsePositives = (p_ns).^n      
-    p_falseNegatives = (p_is).^n      
+    fprintf("=======================\nn=%d\n\n", i)
+    p_falsePositives = (p_ns).^i      
+    p_falseNegatives = (p_is).^i      
 end
+format
 
-
-
+%% 
 % ============
 %  EXERCISE 2
 % ============
 clear
+fprintf("=======================\n\n\n")
 birthRate = [1 20 10 5];
 deathRate = [180 40 20 2];
 
@@ -50,43 +50,28 @@ timePercentage3 = aux2(3)*timePercentage0;
 timePercentage4 = aux2(4)*timePercentage0;
 timePercentage = [timePercentage0 timePercentage1 timePercentage2 timePercentage3 timePercentage4];
 %timePercentage0+timePercentage1+timePercentage2+timePercentage3+timePercentage4
-disp("2.a)")
-disp(timePercentage)
-disp("\n")
+fprintf("2.a) %d\n", timePercentage)
 
 % 2.b
 errorRate = [10^-6 10^-5 10^-4 10^-3 10^-2];
 linkAvgErrorRate = sum(timePercentage.*errorRate);
-disp("2.b)")
-disp(linkAvgErrorRate)
-disp("\n")
-
+fprintf("2.b) %d\n", linkAvgErrorRate)
 
 % 2.c
 avgHoursEachState = [1/birthRate(1) 1/(birthRate(2)+deathRate(1)) 1/(birthRate(3)+deathRate(2)) 1/(birthRate(4)+deathRate(3)) 1/deathRate(4)]; % In hours
 avgMinutesEachState = avgHoursEachState.*60;
-disp("2.c)")
-disp(avgMinutesEachState)
-disp("\n")
-
+fprintf("2.c) %d\n", avgMinutesEachState)
 
 % 2.d
 % States in which the link is in interference state: 4(10^-3) and 5(10^-2)
 probInterferenceState = timePercentage(4) + timePercentage(5);
-disp("2.d)")
-disp(probInterferenceState)
-disp("\n")
-
+fprintf("2.d) %d\n", probInterferenceState)
 
 % 2.e
 avgBitErrorRateInInterferenceState = (timePercentage(4)*errorRate(4)+timePercentage(5)*errorRate(5))/probInterferenceState;
-disp("2.e)")
-disp(avgBitErrorRateInInterferenceState)
-disp("\n")
-
+fprintf("2.e) %d\n",avgBitErrorRateInInterferenceState)
 
 % 2.f
-
-disp("2.f)")
-disp("TODO")
-disp("\n")
+% States in which the link is in interference state: 4(10^-3) and 5(10^-2)
+avgMinutesInterferenceState = avgMinutesEachState(4) + avgMinutesEachState(5);
+fprintf("2.e) %d\n",avgMinutesInterferenceState)
